4b534a980b8a137c14d916e286ea8f50898876b5
Vladimir Sadovnikov <sadko4u@gmail.com>
2021-08-17 00:55:59 +0300

    Updated dependencies

--- project.mk
+++ project.mk
@@ -21,7 +21,7 @@
 # Package version
 ARTIFACT_ID                 = LSP_PLUGINS_LATENCY_METER
 ARTIFACT_NAME               = lsp-plugins-latency-meter
-ARTIFACT_DESC               = Trigger plugin series
+ARTIFACT_DESC               = Latency Meter plugin series
 ARTIFACT_HEADERS            = lsp-plug.in
 ARTIFACT_EXPORT_ALL         = 1
 ARTIFACT_VERSION            = 1.0.1-devel
